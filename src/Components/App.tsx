e7c262fd1ae1a97976075844eae7f2992229e81d
yovel <yovel98@gmail.com>
2021-03-13 19:12:28 +0200

    cleaned some code, added tslint

---
+++ src/Components/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './App.css';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface AppProps {}
+
+const App: React.FC<AppProps> = ({}) => {
+  return (
+    <div className="App">
+      Hello
+  </div>
+  )
+}
+
+export default App;
